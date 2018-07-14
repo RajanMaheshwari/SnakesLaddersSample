0fa8ce371e38726e07cd4c97e27c45dfa3585848
Rajan Maheshwari <rajanmaheshwari1990@gmail.com>
2018-07-14 14:49:08 +0530

    managed multiple players game

--- SnakeAndLadders/View Controllers/Play Scene/SLPlayVC.swift
+++ SnakeAndLadders/View Controllers/Play Scene/SLPlayVC.swift
@@ -17,6 +17,10 @@ class SLPlayVC: UIViewController {
     @IBOutlet weak var playerTwoLabel: UILabel!
     @IBOutlet weak var playerOneLabel: UILabel!
     @IBOutlet weak var multiplierConstraint: NSLayoutConstraint!
+    @IBOutlet weak var playerFourScoreLabel: UILabel!
+    @IBOutlet weak var playerFourLabel: UILabel!
+    @IBOutlet weak var playerThreeScoreLabel: UILabel!
+    @IBOutlet weak var playerThreeLabel: UILabel!
     @IBOutlet weak var boardCollectionView: UICollectionView!
     
     var ratio:CGFloat = 0.0
@@ -55,34 +59,44 @@ class SLPlayVC: UIViewController {
     }
     
     private func initiateGameSetup() {
-        players.forEach { (player:Player) in
+        
+        self.playerOneLabel.text = ""
+        self.playerTwoLabel.text = ""
+        self.playerThreeLabel.text = ""
+        self.playerFourLabel.text = ""
+        
+        for (index,player) in players.enumerated() {
             player.block = nil
             player.score = 0
             player.isMyChance = false
-        }
-        players[0].isMyChance = true
-        self.statusLabel.text = "\(players[0].name) Turn"
+            switch index {
+                
+            case 0:
+                self.playerOneLabel.text = player.name
+                player.isMyChance = true
 
-        playerOne.name = "Player 1"
-        playerTwo.name = "Player 2"
-        playerOne.score = 0
-        playerTwo.score = 0
-        playerOne.block = nil
-        playerTwo.block = nil
-        playerOne.isMyChance = true
-        playerTwo.isMyChance = false
-        self.statusLabel.text = "\(playerOne.name) Turn"
+            case 1:
+                self.playerTwoLabel.text = player.name
+                
+            case 2:
+                self.playerThreeLabel.text = player.name
+                
+            case 3:
+                self.playerFourLabel.text = player.name
+                
+            default:
+                return
+            }
+            self.statusLabel.text = "\(playerOne.name) Turn"
+        }
         self.updateScore()
-        self.playerOneLabel.text = playerOne.name
-        self.playerTwoLabel.text = playerTwo.name
-        
         self.boardCollectionView.reloadData()
     }
     
     //Making the board numbers
     private func makeData() {
         for i in (1...totalRows).reversed() {
-            let maxRowNumber = 7 * i
+            let maxRowNumber = blocksPerRow * i
             if i % 2 == 0 {
                 //even rows
                 for index in stride(from: maxRowNumber, to: (maxRowNumber - blocksPerRow), by: -1) {
@@ -142,32 +156,66 @@ class SLPlayVC: UIViewController {
     }
     
     private func updateScore() {
-        self.playerOneScoreLabel.text = "\(self.playerOne.score)"
-        self.playerTwoScoreLabel.text = "\(self.playerTwo.score)"
         
-        if self.playerOne.score != 0 {
-            if let block1 = blocks.filter({$0.value == playerOne.score}).first {
-            self.playerOne.block = block1
+        for (index,player) in players.enumerated() {
+            //Check if anyone has won
+            if player.score == totalRows * blocksPerRow {
+                self.statusLabel.text = "\(player.name) is WINNER"
+                self.goToWinner(winner: player.name)
+                return
+            }
+            
+            //Assigning block to players
+            if player.score != 0 {
+                if let block = blocks.filter({$0.value == player.score}).first {
+                    player.block = block
+                }
+            }
+            
+            switch index {
+            case 0:
+                self.playerOneScoreLabel.text = "\(player.score)"
+                
+            case 1:
+                self.playerTwoScoreLabel.text = "\(player.score)"
+                
+            case 2:
+                self.playerThreeScoreLabel.text = "\(player.score)"
+
+            case 3:
+                self.playerFourScoreLabel.text = "\(player.score)"
+
+            default:
+                return
             }
         }
         
-        if self.playerTwo.score != 0 {
-            if let block2 = blocks.filter({$0.value == self.playerTwo.score}).first {
-                self.playerTwo.block = block2
-            }
-        }
+//        self.playerOneScoreLabel.text = "\(self.playerOne.score)"
+//        self.playerTwoScoreLabel.text = "\(self.playerTwo.score)"
+        
+//        if self.playerOne.score != 0 {
+//            if let block1 = blocks.filter({$0.value == playerOne.score}).first {
+//            self.playerOne.block = block1
+//            }
+//        }
+//
+//        if self.playerTwo.score != 0 {
+//            if let block2 = blocks.filter({$0.value == self.playerTwo.score}).first {
+//                self.playerTwo.block = block2
+//            }
+//        }
         
         self.boardCollectionView.reloadData()
         
-        if playerOne.score == totalRows * blocksPerRow {
-            self.statusLabel.text = "\(playerOne.name) is WINNER"
-            self.goToWinner(winner: playerOne.name)
-            return
-        }
-        if playerTwo.score == totalRows * blocksPerRow {
-            self.statusLabel.text = "\(playerTwo.name) is WINNER"
-            self.goToWinner(winner: playerTwo.name)
-        }
+//        if playerOne.score == totalRows * blocksPerRow {
+//            self.statusLabel.text = "\(playerOne.name) is WINNER"
+//            self.goToWinner(winner: playerOne.name)
+//            return
+//        }
+//        if playerTwo.score == totalRows * blocksPerRow {
+//            self.statusLabel.text = "\(playerTwo.name) is WINNER"
+//            self.goToWinner(winner: playerTwo.name)
+//        }
     }
     
     func calculateScore(value:Int) -> Int {
@@ -190,31 +238,58 @@ class SLPlayVC: UIViewController {
 
         let diceRoll = Int(arc4random_uniform(6)) + 1
         diceImageView.image = UIImage(named: "Dice\(diceRoll)")
-        if playerOne.isMyChance {
-            self.playerOne.score += diceRoll
-            if playerOne.score > totalRows * blocksPerRow {
-                playerOne.score -= diceRoll
-                self.statusLabel.text = "\(playerOne.name) Can't move"
-            }
-            self.playerOne.score = calculateScore(value: self.playerOne.score)
-            if diceRoll != 6 {
-                playerTwo.isMyChance = true
-                playerOne.isMyChance = false
-                self.statusLabel.text = "\(playerTwo.name) Turn"
-            }
-        } else {
-            self.playerTwo.score += diceRoll
-            if playerTwo.score > totalRows * blocksPerRow {
-                playerTwo.score -= diceRoll
-                self.statusLabel.text = "\(playerTwo.name) Can't move"
-            }
-            self.playerTwo.score = calculateScore(value: self.playerTwo.score)
-            if diceRoll != 6 {
-                playerOne.isMyChance = true
-                playerTwo.isMyChance = false
-                self.statusLabel.text = "\(playerOne.name) Turn"
+        
+        for (index,player) in players.enumerated() {
+            if player.isMyChance {
+                player.score += diceRoll
+                if player.score > totalRows * blocksPerRow {
+                    player.score -= diceRoll
+                    self.statusLabel.text = "\(player.name) Can't move"
+                }
+                player.score = calculateScore(value: player.score)
+                if diceRoll != 6 {
+                    
+                    //Change chance
+                    player.isMyChance = false
+                    if index < players.count - 1 {
+                        players[index+1].isMyChance = true
+                        self.statusLabel.text = "\(players[index+1].name) Turn"
+                    } else {
+                        //It means that it was last player chance. Now moving the pointer back to first player
+                        players[0].isMyChance = true
+                        self.statusLabel.text = "\(players[0].name) Turn"
+                    }
+                    break
+                }
             }
         }
+        
+        
+//        if playerOne.isMyChance {
+//            self.playerOne.score += diceRoll
+//            if playerOne.score > totalRows * blocksPerRow {
+//                playerOne.score -= diceRoll
+//                self.statusLabel.text = "\(playerOne.name) Can't move"
+//            }
+//            self.playerOne.score = calculateScore(value: self.playerOne.score)
+//            if diceRoll != 6 {
+//                playerTwo.isMyChance = true
+//                playerOne.isMyChance = false
+//                self.statusLabel.text = "\(playerTwo.name) Turn"
+//            }
+//        } else {
+//            self.playerTwo.score += diceRoll
+//            if playerTwo.score > totalRows * blocksPerRow {
+//                playerTwo.score -= diceRoll
+//                self.statusLabel.text = "\(playerTwo.name) Can't move"
+//            }
+//            self.playerTwo.score = calculateScore(value: self.playerTwo.score)
+//            if diceRoll != 6 {
+//                playerOne.isMyChance = true
+//                playerTwo.isMyChance = false
+//                self.statusLabel.text = "\(playerOne.name) Turn"
+//            }
+//        }
         self.updateScore()
     }
     
